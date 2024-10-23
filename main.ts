635266ca1f993b965ccee5cc0c053ad18d965cdb
Corbyn-daGama <corbyn.da-gama@mths.ca>
2024-10-23 11:21:28 -0400

    made a program that can rotate a servo motor

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Corbyn da Gama
+ * Created on: Oct 2024
+ * This program moves the servo back and forth
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// rotate servo to 34 degrees
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 34)
+    basic.clearScreen()
+    basic.showString('34')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// rotate servo to 167 degrees
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 167)
+    basic.clearScreen()
+    basic.showString('167')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
